99814fc30039a4ce793eaeae42ea3c59b3ac1839
JaoCastelar <joao.castelar@estudante.iftm.edu.br>
2023-04-18 17:47:19 -0300

    lista_5_18-04-2023

---
+++ lista5/script1.js
@@ -0,0 +1,5 @@
+let btn = document.getElementById("btn");
+btn.addEventListener("click", function() {
+    let nome = document.getElementById("nome");
+    nome.innerHTML = "João Pedro da Silva Castelar"
+});
